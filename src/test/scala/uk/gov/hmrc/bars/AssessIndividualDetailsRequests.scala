248e42d4d78658f4d81ffa08addb2921c3874f06
Mark Collin <1205116+Ardesco@users.noreply.github.com>
2020-04-07 11:07:50 +0100

    #DLK-311 Add individual performance test

---
+++ src/test/scala/uk/gov/hmrc/bars/AssessIndividualDetailsRequests.scala
@@ -0,0 +1,37 @@
+package uk.gov.hmrc.bars
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import uk.gov.hmrc.performance.conf.ServicesConfiguration
+
+object AssessIndividualDetailsRequests extends ServicesConfiguration {
+
+  val baseUrl = s"${baseUrlFor("bank-account-reputation")}/bank-account-reputation"
+  val csrfPattern = """<input type="hidden" name="csrfToken" value="([^"]+)""""
+
+  val assessIndividualBankDetails = {
+    http("Submit sort code, account number, name and post code")
+      .post(s"$baseUrl/individual/v2/assess": String)
+      .header("Content-Type", "application/json")
+      .body(StringBody(
+        """|{
+           |  "account": {
+           |    "sortCode": "${sortCode}",
+           |    "accountNumber": "${accountNumber}"
+           |  },
+           |  "subject": {
+           |    "name": "${title} ${firstname} ${surname}",
+           |    "address": {
+           |      "lines": [
+           |      "${flatNumber}${streetNumber} ${street}",
+           |      "${town}"
+           |      ],
+           |      "postcode": "${postcode}"
+           |    }
+           |  }
+           |}
+           |""".stripMargin)).asJSON
+      .check(substring(""""accountExists":"${accountexists}""""))
+      .check(status.is(200))
+  }
+}
